53050e971f99d77ed478c0980adc884e981c48b0
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-04-18 20:23:02 +1000

    feat: added button clicks for real and fake form

--- favouriteAnimals.js
+++ favouriteAnimals.js
@@ -19,9 +19,11 @@ let animals = [
 ];
 
 function createAnimalList(){
-
 	animals = [...new Set(animals)];
 
+	// 0. Find the existing list HTML and reset it
+	let rootOlNode = document.querySelector("ol");
+	rootOlNode.innerHTML = "";
 
 	animals.forEach((animal) => {
 		console.log("animal: " + animal);
@@ -34,21 +36,16 @@ function createAnimalList(){
 
 		// 1b. Add a button to remove the element from the list 
 		let removeItemButton = document.createElement("button");
-
 		removeItemButton.onclick = (() => removeAnimalFromList(animal));
-
 		removeItemButton.textContent = "Remove animal";
-
 		newList.appendChild(removeItemButton);
 
-
 		// 2. Find the ol element that exists in the page and append the li into it
-		let rootOlNode = document.querySelector("ol");
+		
 		rootOlNode.appendChild(newList);
 
 	});
 }
-
 
 function removeAnimalFromList(targetAnimalId){
 	// 1. Find element in list with matching ID 
@@ -79,9 +76,29 @@ function removeAnimalFromList(targetAnimalId){
 
 }
 
+function addAnimalToList(event, targetInputId) {
+	event.preventDefault(); //prevents form submission from refreshing the page
+	// 1. Find the input field matching targetInputId
+	let targetInputField = document.getElementById(targetInputId);
 
+	// 2 .Get the text value from the input field that we found
+	let foundInputFieldValue = targetInputField.Value;
+	console.log(`Input field value to add to list is: ${foundInputFieldValue}`);
 
+	// 3. Push the text value into the animals array
+	animals.push(foundInputFieldValue);
 
+	// 4. Recreate the animal list
+	createAnimalList();
+	// Make sure this function deletes the exisiting list first!
+
+}
+
+let fakeFormButton = document.getElementById("fakeform-submit");
+fakeFormButton.addEventListener("click", (event) => {addAnimalToList(event, "fakeform-addAnimal")});
+
+let realFormButton = document.getElementById("realform-submit");
+realFormButton.addEventListener("click", (event) => {addAnimalToList(event, "realform-addAnimal")});
 // createAnimalList();
 
 
